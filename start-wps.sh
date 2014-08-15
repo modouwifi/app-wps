1455b38b20ba4429182993fb7bb15eb88d8f1b87
eric-zhou <zhouyao@mochui.net>
2014-08-15 10:50:08 +0800

    修改路径配置

--- start-wps.sh
+++ start-wps.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+LOOPCMD=$(dirname $0)/loop.sh
+
 /system/sbin/wps.sh 
-textview WPS连接 "" 1000 31 "/data/loop.sh %LOOP-COUNT% >/tmp/out" /tmp/out
+
+textview WPS连接 "" 1000 31 "$LOOPCMD %LOOP-COUNT% >/tmp/out" /tmp/out
