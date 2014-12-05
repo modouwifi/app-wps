fed71b62473a55c16ed78524aa53e62bdada0f80
eric-zhou <zhouyao@mochui.net>
2014-12-05 15:58:46 +0800

    修改进度条显示问题

--- calc-progress.sh
+++ calc-progress.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-SECONDS=30
+COUNT_FILE=/tmp/wps-count
+PERCENT_FILE/tmp/wps-percent
 
-if [ ! -f /tmp/wps-count ] ; then
-    echo 0 > /tmp/wps-count
+if [ ! -f $COUNT_FILE ] ; then
+    echo 0 > $COUNT_FILE
 fi
 
-n=`cat /tmp/wps-count`
-# echo "请在 $n 秒内按下设备上的wps键，如果已经连接成功，可以选择退出"
+n=`cat $COUNT_FILE`
+n=`expr $n + 1`
+echo $n > $COUNT_FILE
+
 let "p = n * 100 / 30"
-echo $p
+if [ $p -ge 100 ] ; then
+    p=100
+fi
+echo $p | tee $PERCENT_FILE
 
-r=`expr $n + 1`
-echo $r > /tmp/wps-count
 
-# if [ $r -ge $SECONDS ] ; then
-#     rm /tmp/wps-count
-# fi
